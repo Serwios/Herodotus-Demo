476afdfb75e8896d1fc3b8473691c20d5a402290
Serwio <kucelas@ukr.net>
2021-02-08 00:03:33 +0200

    Test photo changed

--- src/main/python/FaceRecognizer.py
+++ src/main/python/FaceRecognizer.py
@@ -2,39 +2,28 @@ import cv2
 import numpy as np
 import os
 
-#IMPORT ALL IMAGES
-path = 'ImagesQuery'
+
+PATH = 'ImagesQuery'
 images = []
 classNames = []
-myList = os.listdir(path)
+photoNameList = os.listdir(PATH)
 
-print(myList)
-print('I found objects: ', len(myList))
-print('\n')
+print('Len of photos list \n', len(photoNameList))
 
-for c1 in myList:
-	imgCur = cv2.imread(f'{path}/{c1}')
+for c1 in photoNameList:
+	imgCur = cv2.imread(f'{PATH}/{c1}')
 	images.append(imgCur)
 	print(c1)
-	print('\n')
 	classNames.append(os.path.splitext(c1)[0])
 
-# SHOW IMGS
-# while index < len(images):
-# 	cv2.imshow("img3", images[index])
-# 	index= index + 1
-# 	cv2.waitKey(0)
-
-#MAKE FROM THEM DESKRIPTORS AND KEYPOINTS
 orb = cv2.ORB_create(nfeatures=10000)
 camera_out = cv2.imread('Imagebuf_cam_test.png')
 camera_out_orig = cv2.imread('Imagebuf_cam_test.png')
-#cv2.imshow("img1", camera_out)
-#cv2.waitKey(0)
 kp1, des1 = orb.detectAndCompute(camera_out, None)
-
+    
 def findDes(images):
-	desList=[]
+	desList = []
+	
 	for img in images:
 		kp, des = orb.detectAndCompute(img, None)
 		desList.append(des)
@@ -45,6 +34,7 @@ def findID(img, desList, thresh):
 	bf = cv2.BFMatcher()
 	matchList = []
 	finalVal = -1
+	
 	for des in desList:
 		matches = bf.knnMatch(des, des2, k=2)
 		good = []
@@ -55,22 +45,18 @@ def findID(img, desList, thresh):
 		print(matchList)
 			
 	
-	if len(matchList)!=0:
+	if len(matchList) != 0:
 		if max(matchList) > thresh:
 			finalVal = matchList.index(max(matchList))
-			print(finalVal)
+			print("\nFinal val: ", finalVal)
 	return 	finalVal
 
 desList = findDes(images)
-id = findID(camera_out, desList, 3000)
-print("IT WAS HIM = ", classNames[id])
+id = findID(camera_out, desList, 1)
+print("It was =", classNames[id])
 
 if id > -1:
-	#print(camera_out_orig, classNames[id])
-	absolute_path = 'ImagesQuery/'+classNames[id]+'.png'
-	#print(zero_path)
-	zero_read = cv2.imread(absolute_path)
-	#print(zero_read)
+	zero_read = cv2.imread('ImagesQuery/' + classNames[id] + '.png')
 	kp2, des2 = orb.detectAndCompute(zero_read, None)
 	bf = cv2.BFMatcher()
 	good = []
@@ -82,5 +68,4 @@ if id > -1:
 	compared_images = cv2.drawMatchesKnn(camera_out_orig, kp1, zero_read, kp2, good, None, flags=2)
 	cv2.imshow("img3", compared_images)
 	cv2.waitKey(0)
-
-	print("Hello")
+	
